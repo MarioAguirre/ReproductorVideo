9596dea570743c284057f906b3f42e7421b400ad
MarioAguirre <marioaguirreleyton6@gmail.com>
2022-11-10 00:44:44 -0300

    Initial commit

---
+++ app/src/main/java/com/aguirre/reproductorvideo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.aguirre.reproductorvideo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView mivideo = findViewById(R.id.vView);
+
+        String videop ="android.resource://" + getPackageName() + "/" + R.raw.video;
+        Uri uri = Uri.parse(videop);
+        mivideo.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        mivideo.setMediaController(mediaController);
+        mediaController.setAnchorView(mivideo);
+    }
+}
